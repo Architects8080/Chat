75b9d4eb85158c5fe01b4452c7b8c0700e41c5a4
sujlee <sujlee@student.42seoul.kr>
2021-08-24 16:18:40 +0900

    FEAT: #11 generate DM entity

---
+++ backend/src/dm/entity/dm.entity.ts
@@ -0,0 +1,32 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn} from 'typeorm';
+import { User } from "../../user/entity/user.entity";
+
+@Entity('direct_message')
+export class DirectMessage {
+  @PrimaryGeneratedColumn()
+  dmID: number;
+
+  @Column({ nullable: false, length: 255 })
+  message: string;
+
+  @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP(6)"})
+  timestamp: Date;
+}
+
+@Entity('direct_message_info')
+export class DirectMessageInfo {
+  @ManyToOne(() => DirectMessage, dmID => dmID.dmID, { nullable: false })
+  @JoinColumn({ name: 'dmID' })
+  dmID: DirectMessage;
+
+  @ManyToOne(() => User, userID => userID.id, { primary: true, })
+  @JoinColumn({ name: 'userID' })
+  userID: User;
+
+  @ManyToOne(() => User, friendID => friendID.id, { primary: true, })
+  @JoinColumn({ name: 'friendID' })
+  friendID: User;
+
+  @Column({ nullable: false })
+  is_sender: boolean;
+}
